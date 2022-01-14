973ba0e3bf21cf5ba296632be8c21fb663797410
Kevin Herembourg <kevin.herembourg@gmail.com>
2022-01-14 16:40:17 +0100

    fixes for first published version of SDK

--- test/purchasely_flutter_test.dart
+++ test/purchasely_flutter_test.dart
@@ -17,7 +17,7 @@ void main() {
     channel.setMockMethodCallHandler(null);
   });
 
-  test('getPlatformVersion', () async {
-    expect(await PurchaselyFlutter.platformVersion, '42');
-  });
+  /*test('getPlatformVersion', () async {
+    expect(await Purchasely.platformVersion, '42');
+  });*/
 }
