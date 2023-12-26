6924128258ba81ec2aabf5c5daf17adac9143282
chouaibMo <chouaib@purchasely.com>
2023-12-26 18:24:14 +0100

    Version 4.2.1

--- purchasely/android/src/main/kotlin/io/purchasely/purchasely_flutter/PurchaselyFlutterPlugin.kt
+++ purchasely/android/src/main/kotlin/io/purchasely/purchasely_flutter/PurchaselyFlutterPlugin.kt
@@ -329,7 +329,7 @@ class PurchaselyFlutterPlugin: FlutterPlugin, MethodCallHandler, ActivityAware, 
             .userId(userId)
             .build()
 
-	  Purchasely.sdkBridgeVersion = "4.2.0"
+	  Purchasely.sdkBridgeVersion = "4.2.1"
         Purchasely.appTechnology = PLYAppTechnology.FLUTTER
 
         Purchasely.start { isConfigured, error ->
--- purchasely/ios/Classes/SwiftPurchaselyFlutterPlugin.swift
+++ purchasely/ios/Classes/SwiftPurchaselyFlutterPlugin.swift
@@ -208,7 +208,7 @@ public class SwiftPurchaselyFlutterPlugin: NSObject, FlutterPlugin {
             return
         }
 
-		Purchasely.setSdkBridgeVersion("4.2.0")
+		Purchasely.setSdkBridgeVersion("4.2.1")
         Purchasely.setAppTechnology(PLYAppTechnology.flutter)
 
         let logLevel = PLYLogger.LogLevel(rawValue: (arguments["logLevel"] as? Int) ?? PLYLogger.LogLevel.debug.rawValue) ?? PLYLogger.LogLevel.debug
