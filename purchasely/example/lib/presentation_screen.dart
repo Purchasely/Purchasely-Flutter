522108e62ebb7c6c31c05b3ce8e9a793cc4f9e77
chouaibMo <chouaib@purchasely.com>
2024-01-19 17:16:42 +0100

    Add interceptor in view presentation screen

--- purchasely/example/lib/presentation_screen.dart
+++ purchasely/example/lib/presentation_screen.dart
@@ -15,6 +15,37 @@ class PresentationScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
 
+    Purchasely.setPaywallActionInterceptorCallback((PaywallActionInterceptorResult result) {
+          print('Received action from paywall');
+          inspect(result);
+
+          if (result.action == PLYPaywallAction.navigate) {
+            print('User wants to navigate');
+            Purchasely.onProcessAction(true);
+          } else if (result.action == PLYPaywallAction.close) {
+            print('User wants to close paywall');
+            Purchasely.onProcessAction(true);
+          } else if (result.action == PLYPaywallAction.login) {
+            print('User wants to login');
+            //Present your own screen for user to log in
+            onClose();
+            Purchasely.userLogin('MY_USER_ID');
+            Purchasely.onProcessAction(true);
+          } else if (result.action == PLYPaywallAction.open_presentation) {
+            print('User wants to open a new paywall');
+            Purchasely.onProcessAction(true);
+          } else if (result.action == PLYPaywallAction.purchase) {
+            print('User wants to purchase');
+            Purchasely.onProcessAction(true);
+          } else if (result.action == PLYPaywallAction.restore) {
+            print('User wants to restore his purchases');
+            Purchasely.onProcessAction(true);
+          } else {
+            print('Action unknown ' + result.action.toString());
+            Purchasely.onProcessAction(true);
+          }
+        });
+
     PLYPresentationView? presentationView = Purchasely.getPresentationView(
       onLoaded: onLoaded,
       onClose: onClose,
