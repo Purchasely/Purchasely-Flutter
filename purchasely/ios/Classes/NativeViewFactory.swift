5deee7e0abfb211845181001b918a63f1a0fb7c2
chouaibMo <chouaib@purchasely.com>
2024-01-23 16:34:01 +0100

    Native view: ios platform side

---
+++ purchasely/ios/Classes/NativeViewFactory.swift
@@ -0,0 +1,31 @@
+import Foundation
+import Flutter
+import UIKit
+import Purchasely
+
+class NativeViewFactory: NSObject, FlutterPlatformViewFactory {
+    private var messenger: FlutterBinaryMessenger
+
+    init(messenger: FlutterBinaryMessenger) {
+        self.messenger = messenger
+        super.init()
+    }
+
+    func create(
+        withFrame frame: CGRect,
+        viewIdentifier viewId: Int64,
+        arguments args: Any?
+    ) -> FlutterPlatformView {
+        
+        return NativeView(
+            frame: frame,
+            viewIdentifier: viewId,
+            arguments: args,
+            binaryMessenger: messenger)
+    }
+
+    /// Implementing this method is only necessary when the `arguments` in `createWithFrame` is not `nil`.
+    public func createArgsCodec() -> FlutterMessageCodec & NSObjectProtocol {
+          return FlutterStandardMessageCodec.sharedInstance()
+    }
+}
