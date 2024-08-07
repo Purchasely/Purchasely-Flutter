68755dc50ceb1d4ca5735ca346dd73b299a847e5
Kevin Herembourg <kevin.herembourg@gmail.com>
2024-08-07 15:04:52 +0200

    update version everywhere

--- purchasely/ios/Classes/SwiftPurchaselyFlutterPlugin.swift
+++ purchasely/ios/Classes/SwiftPurchaselyFlutterPlugin.swift
@@ -283,7 +283,7 @@ public class SwiftPurchaselyFlutterPlugin: NSObject, FlutterPlugin {
             return
         }
 
-		Purchasely.setSdkBridgeVersion("4.4.1")
+		Purchasely.setSdkBridgeVersion("4.5.0")
         Purchasely.setAppTechnology(PLYAppTechnology.flutter)
 
         let logLevel = PLYLogger.LogLevel(rawValue: (arguments["logLevel"] as? Int) ?? PLYLogger.LogLevel.debug.rawValue) ?? PLYLogger.LogLevel.debug
