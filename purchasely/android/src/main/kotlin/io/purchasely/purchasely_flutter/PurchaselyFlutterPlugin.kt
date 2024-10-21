8c9368335e36b08acedcafdef7e4f258d20bedeb
Chouaib Mounaime <chouaib@purchasely.com>
2024-10-21 16:36:15 +0200

    Version 4.5.1 (#45)
    * Bump Android and iOS SDKs
    
    * Update bridge version for iOS

--- purchasely/android/src/main/kotlin/io/purchasely/purchasely_flutter/PurchaselyFlutterPlugin.kt
+++ purchasely/android/src/main/kotlin/io/purchasely/purchasely_flutter/PurchaselyFlutterPlugin.kt
@@ -343,7 +343,7 @@ class PurchaselyFlutterPlugin: FlutterPlugin, MethodCallHandler, ActivityAware, 
             .userId(userId)
             .build()
 
-	  Purchasely.sdkBridgeVersion = "4.5.0"
+	  Purchasely.sdkBridgeVersion = "4.5.1"
         Purchasely.appTechnology = PLYAppTechnology.FLUTTER
 
         Purchasely.start { isConfigured, error ->
--- purchasely_android_player/example/android/app/src/main/kotlin/io/purchasely/sample/player/purchasely_android_player_example/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package io.purchasely.sample.player.purchasely_android_player_example
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
