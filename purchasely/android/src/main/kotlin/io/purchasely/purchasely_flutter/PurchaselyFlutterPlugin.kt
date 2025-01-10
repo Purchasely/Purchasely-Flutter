b991b40ebff48bdf6cce958f1d9a2921d9f998b4
chouaibMo <chouaib@purchasely.com>
2025-01-10 14:34:20 +0100

    Version 5.0.2

--- purchasely/android/src/main/kotlin/io/purchasely/purchasely_flutter/PurchaselyFlutterPlugin.kt
+++ purchasely/android/src/main/kotlin/io/purchasely/purchasely_flutter/PurchaselyFlutterPlugin.kt
@@ -363,7 +363,7 @@ class PurchaselyFlutterPlugin: FlutterPlugin, MethodCallHandler, ActivityAware, 
             .userId(userId)
             .build()
 
-	  Purchasely.sdkBridgeVersion = "5.0.1"
+	  Purchasely.sdkBridgeVersion = "5.0.2"
         Purchasely.appTechnology = PLYAppTechnology.FLUTTER
 
         Purchasely.start { isConfigured, error ->
